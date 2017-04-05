5e89f9083843b07b103bba845811b67dda5ee319
Bentzkast <jalfredopoly@gmail.com>
2017-04-05 15:44:53 -0700

    6counter

--- 6bitCounter/6bitCounter.c
+++ 6bitCounter/6bitCounter.c
@@ -5,26 +5,15 @@ int main(int argc, char *argv[])
 {
    printf("D5 D4 D3 D2 D1 D0\n");
    printf("-----------------\n");
-   int a,b,c,d,e,f;
+   int i,j;
 
-   for(a = 0; a < 2;a++)
+   for(i = 0; i < 64;i++)
    {
-      for(b = 0; b < 2;b++)
+      for(j = 0; j < 6;j++)
       {
-         for(c = 0; c < 2;c++)
-         {
-            for(d = 0; d < 2;d++)
-            {
-               for(e = 0;e < 2;e++)
-               {
-                  for(f = 0;f < 2;f++)
-                  {
-                     printf("%d  %d  %d  %d  %d  %d\n",a,b,c,d,e,f);
-                  }
-               }
-            }
-         }
+         printf("%d  ",(i >> (5 - j)) & 1);
       }
+      printf("\n");
    }
    printf("0  0  0  0  0  0\n");
    return 0;
